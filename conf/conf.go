8f531ce86f388e360464562544fc3b1b0c97c8f3
bian <bian@test>
2018-09-14 10:28:40 +0800

    init.

--- conf/conf.go
+++ conf/conf.go
@@ -1,11 +1,1 @@
 package conf
-
-import (
-	"github.com/BurntSushi/toml"
-}
-
-type LogConfig struct {
-	Enable    bool   `toml:"enable"`
-	Timestamp bool   `toml:"timestamp"`
-	Dir       string `toml:"dirpath"`
-}
