f63b5940c978213bcd825698c6fd4ca7a0b0f92c
bian <bian@test>
2018-09-29 15:18:35 +0800

    [Update] add const.

--- module/base.go
+++ module/base.go
@@ -6,27 +6,23 @@ import (
 	"github.com/bypdhu/ssh-executor/result"
 	"github.com/bypdhu/ssh-executor/conf"
 	"github.com/bypdhu/ssh-executor/module/shell"
-)
-
-const (
-	MODULE_SHELL = "shell"
-	MODULE_COPY = "copy"
+	"github.com/bypdhu/ssh-executor/common"
 )
 
 var (
 	wg sync.WaitGroup
 )
 
-func RunAll(c *conf.Config, hs []string, r map[string]*result.SSHResult) {
+func RunAll(c *conf.Config, hs []string, r map[string]*result.BaseResult) {
 
 	for _, h := range hs {
-		r[h] = &result.SSHResult{}
+		r[h] = &result.BaseResult{}
 
 		wg.Add(1)
 		switch c.Direct.Module {
-		case MODULE_SHELL:
+		case common.MODULE_SHELL.String():
 			go runShell(c, h, r[h], &wg)
-		case MODULE_COPY:
+		case common.MODULE_COPY.String():
 			go runCopy(c, h, r[h], &wg)
 		default:
 			go runShell(c, h, r[h], &wg)
@@ -36,11 +32,11 @@ func RunAll(c *conf.Config, hs []string, r map[string]*result.SSHResult) {
 
 }
 
-func runShell(c *conf.Config, h string, r *result.SSHResult, w *sync.WaitGroup) {
+func runShell(c *conf.Config, h string, r *result.BaseResult, w *sync.WaitGroup) {
 	defer w.Done()
 	shell.Run(c, h, r)
 }
 
-func runCopy(c *conf.Config, h string, r *result.SSHResult, w *sync.WaitGroup) {
+func runCopy(c *conf.Config, h string, r *result.BaseResult, w *sync.WaitGroup) {
 	defer w.Done()
 }
--- module/shell/base.go
+++ module/shell/base.go
@@ -6,14 +6,14 @@ import (
 	"github.com/bypdhu/ssh-executor/conf"
 )
 
-func Run(c *conf.Config, h string, result *result.SSHResult) {
+func Run(c *conf.Config, h string, result *result.BaseResult) {
 
 	client := bssh.New(h, 22, c.Direct.UserName, c.Direct.Password)
 	//log.Infof("+++++++++now run %s on host %s\n", c.Direct.Command, h)
 	//log.Infof("client is %s", client)
-	result.Err = client.Run(c.Direct.Command)
-	result.Result = client.Session.Result
-	result.ExitCode = client.Session.ExitCode
+	result.Err = client.RunCommand(c.Direct.Command)
+	result.Result = client.Result
+	result.ExitCode = client.ExitCode
 
 	//log.Infof("++++++result is %s on host %s\n", result.result, h)
 }
