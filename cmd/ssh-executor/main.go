fbe6d435ba73c350fccbcce4ad42f27ab2235c73
bian <bian@test>
2018-10-09 11:00:23 +0800

    [Update] run many hosts.

--- cmd/ssh-executor/main.go
+++ cmd/ssh-executor/main.go
@@ -11,14 +11,15 @@ import (
 
 	"github.com/bypdhu/ssh-executor/cmd/ssh-executor/flags"
 	"github.com/bypdhu/ssh-executor/conf"
-	"github.com/bypdhu/ssh-executor/utils"
-	"github.com/bypdhu/ssh-executor/result"
 	"github.com/bypdhu/ssh-executor/module"
+	"github.com/bypdhu/ssh-executor/common"
+	"github.com/bypdhu/ssh-executor/task"
 )
 
 var (
 	c *conf.Config
-	results map[string]*result.BaseResult
+	cs map[string]*conf.Config
+	//rs map[string]*result.DirectResult
 )
 
 func main() {
@@ -33,9 +34,9 @@ func main() {
 	//log.Infof("==========launch type: %s\n", c.LaunchType)
 
 	switch c.LaunchType {
-	case "direct":
+	case common.LAUNCH_DIRECT.String(), strings.ToLower(common.LAUNCH_DIRECT.String()):
 		doDirect(c)
-	case "server":
+	case common.LAUNCH_SERVER.String(), strings.ToLower(common.LAUNCH_SERVER.String()):
 		log.Infof("Starting up %s ...\n", version.Print())
 		log.Infoln(fs.PrintAll())
 		log.Infof("Final config, override by command-args: %s", c)
@@ -49,28 +50,30 @@ func doServer(c *conf.Config) {
 }
 
 func doDirect(c *conf.Config) {
-	results = make(map[string]*result.BaseResult)
 
-	hosts := getHosts(c)
+	addTask(c)
 
-	module.RunAll(c, hosts, results)
+	hs := conf.GetHosts(c)
 
-	for _, host := range hosts {
-		fmt.Printf("host:%s, result:%s, exitCode:%d, err:%s\n", host, results[host].Result, results[host].ExitCode, results[host].Err)
-	}
-}
+	cs = conf.GetCopiedConfigMap(c, hs)
 
-func getHosts(c *conf.Config) ([]string) {
-	hosts := strings.Split(c.Direct.Hosts, ",")
-	if c.Direct.HostsFile != "" {
-		hosts_from_file, err := utils.ReadLineNotEmpty(c.Direct.HostsFile)
-		if err != nil {
-			log.Fatal(err)
+	module.RunAll(cs)
+
+	for h, c := range cs {
+		fmt.Printf("+++++++host:%s\n", h)
+		for _, t := range c.Tasks {
+			fmt.Printf("task:%+v\n", t)
 		}
-		hosts = append(hosts, hosts_from_file...)
 	}
-
-	hosts = utils.RemoveDupString(hosts)
-
-	return hosts
 }
+
+func addTask(c *conf.Config) {
+	if c.Direct.Command != "" {
+		_t := task.DefaultTask(common.MODULE_SHELL)
+		_t.Command = c.Direct.Command
+		c.Tasks = append(c.Tasks, _t)
+		copy(c.Tasks[1:], c.Tasks[0:len(c.Tasks) - 1])
+		c.Tasks[0] = _t
+	}
+}
+
